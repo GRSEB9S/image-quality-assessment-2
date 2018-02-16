06d10521985f48a7efd98a0a05880422b21a4a9a
Dheeraj R Reddy <dheeraj98reddy@gmail.com>
2018-02-16 22:31:34 +0530

    Broken state
    Efficient way to extract patches, either in the dataset or at the start of each forward prop
    Model saving is pending

---
+++ network.py
@@ -0,0 +1,74 @@
+import numpy as np
+import tensorflow as tf
+import tf_util as U
+
+class Model:
+    def __init__(self, loss = "weighted"):
+        self.loss = loss
+        self.prob = tf.placeholder(shape=(), dtype=tf.float32)
+        self.lr = tf.placeholder(shape=(), dtype=tf.float32)
+    
+    def block(x, filters):
+        net = U.conv2d(net, filters, 'conv1', (3, 3))
+        net = U.swish(net)
+        net = U.conv2d(net, filters, 'conv2', (3, 3))
+        net = U.swish(net)
+        net = U.maxpool(net, 2)
+        return net
+
+    def build(image, mos_score):
+
+        print(mos_score)
+        self.n_images  = tf.shape(mos_score)[0]
+        self.n_patches = tf.shape(image)[0]
+
+        net = block(net, 32)
+        net = block(net, 64)
+        net = block(net, 128)
+        net = block(net, 256)
+        net = block(net, 512)
+
+        net1 = tf.reshape(net, (-1, 512))
+        net1 = U.dense(net1, 512)
+        net1 = U.swish(net1)
+        net1 = tf.nn.dropout(net1, keep_prob = self.prob)
+        net1 = U.dense(net1, 1)
+
+        if self.loss == "patchwise":
+            mos_score = tf.tile(mos_score, self.n_patches)
+            self.loss_op = patchwise_loss(net1, mos_score)
+            self.output = net1
+        
+        elif self.loss == "weighted":
+            net2 = tf.reshape(net, (-1, 512))
+            net2 = U.dense(net2, 512)
+            net2 = U.swish(net2)
+            net2 = tf.nn.dropout(net2, keep_prob = self.prob)
+            net2 = U.dense(net2, 1)
+            net2 = tf.nn.relu(net2) + 1e-6
+            self.loss_op = weighted_loss(net1, net2, mos_score)
+
+        optimizer = tf.train.AdamOptimizer(lr)
+        self.train_op = optimizer.minimize(self.loss_op)
+
+        
+    def patchwise_loss(self, h, t):
+        t = U.repeat(t, self.n_patches//self.n_images)
+        loss = tf.reduce_sum(tf.abs(h - t))
+        return loss
+
+    def weighted_loss(self, h, a, t):
+        loss = 0
+        
+        h = tf.split(h, self.n_images, 0)
+        a = tf.split(a, self.n_images, 0)
+        t = tf.split(t, self.n_images, 0)
+
+        for i in range(self.n_images):
+            y = tf.reduce_sum(h[i] * a[i], 0) /  tf.reduce_sum(a[i], 0)
+            self.output += y
+            loss += tf.abs(y - t[i])
+        
+        loss /= self.n_images
+        
+        return loss
---
+++ train.py
@@ -0,0 +1,37 @@
+from network import Model
+import tensorflow as tf
+import numpy as np
+import logger
+from os.path import basename, splitext
+import matplotlib.pyplot as plt
+
+EPOCHS = 1
+BATCHES = 1
+filenames = tf.constant(["test_images/macula.jpg"], dtype=tf.string)
+labels = tf.constant([1], dtype=tf.float32)
+
+logger.configure('/tmp')
+sess = tf.Session()
+def _parse_function(filename, label):
+    image_string = tf.read_file(filename)
+    image_decoded = tf.image.decode_jpeg(image_string, channels=3)
+    image_resized = tf.image.resize_images(image_decoded, [256, 256])
+    image_normal = tf.image.per_image_standardization(image_resized)
+    image_ran_lr = tf.image.random_flip_left_right(image_normal)
+    image_ran_ud = tf.image.random_flip_up_down(image_ran_lr)
+    image_expand_dims = tf.reshape(image_ran_ud, shape=[1, -1])
+    return image_ran_ud, label
+
+model = Model('weighted')
+
+dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
+dataset = dataset.prefetch(100)
+dataset = dataset.map(_parse_function, 10)
+dataset = dataset.shuffle(100)
+dataset = dataset.repeat(EPOCHS)
+dataset = dataset.batch(BATCHES)
+
+itere = dataset.make_one_shot_iterator()
+print(itere.get_next())
+
+model.build(itere.get_next())
