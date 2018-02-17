de2f7f4742fe470edea87b70d9db11ddeb00d686
Dheeraj R Reddy <dheeraj98reddy@gmail.com>
2018-02-17 13:12:57 +0530

    Image is broken into patches on CPU at dataset creation using tf.extract_image_patches

--- network.py
+++ network.py
@@ -1,72 +1,76 @@
-import numpy as np
 import tensorflow as tf
 import tf_util as U
 
+
 class Model:
-    def __init__(self, BATCHES, loss = "weighted"):
-        self.loss = loss
-        self.prob = tf.placeholder(shape=(), dtype=tf.float32)
-        self.lr = tf.placeholder(shape=(), dtype=tf.float32)
-        self.n_images = BATCHES
-        self.n_patches = self.n_images * 64
-    
-    def block(self, net, filters):
-        with tf.variable_scope('block_'+str(filters)):
-            net = U.conv2d(net, filters, 'conv1', (3, 3))
-            net = U.swish(net)
-            net = U.conv2d(net, filters, 'conv2', (3, 3))
-            net = U.swish(net)
-            net = U.maxpool(net, 2)
-        return net
+	def __init__(self, batches, loss = "weighted"):
+		self.loss = loss
+		self.prob = tf.placeholder(shape = (), dtype = tf.float32)
+		self.lr = tf.placeholder(shape = (), dtype = tf.float32)
+		self.n_images = batches
+		self.n_patches = self.n_images * 64
+		self.output = self.loss_op = self.train_op = None
 
-    def build(self, image, mos_score):
-        net = self.block(image, 32)
-        net = self.block(net, 64)
-        net = self.block(net, 128)
-        net = self.block(net, 256)
-        net = self.block(net, 512)
+	@staticmethod
+	def block(net, filters):
+		with tf.variable_scope('block_' + str(filters)):
+			net = U.conv2d(net, filters, 'conv1', (3, 3))
+			net = U.swish(net)
+			net = U.conv2d(net, filters, 'conv2', (3, 3))
+			net = U.swish(net)
+			net = U.maxpool(net, 2)
+		return net
 
-        net1 = tf.reshape(net, (-1, 512))
-        net1 = U.dense(net1, 512, 'fc1')
-        net1 = U.swish(net1)
-        net1 = tf.nn.dropout(net1, keep_prob = self.prob)
-        net1 = U.dense(net1, 1, 'fc2')
+	def build(self, image, mos_score):
+		image.set_shape((self.n_images, 64, 32, 32, 3))
+		net = tf.reshape(image, [self.n_patches, 32, 32, 3])
 
-        if self.loss == "patchwise":
-            mos_score = tf.tile(mos_score, self.n_patches)
-            self.loss_op = self.patchwise_loss(net1, mos_score)
-            self.output = net1
-        
-        elif self.loss == "weighted":
-            net2 = tf.reshape(net, (-1, 512))
-            net2 = U.dense(net2, 512, 'fc1_weight')
-            net2 = U.swish(net2)
-            net2 = tf.nn.dropout(net2, keep_prob = self.prob)
-            net2 = U.dense(net2, 1, 'fc2_weight')
-            net2 = tf.nn.relu(net2) + 1e-6
-            self.loss_op = self.weighted_loss(net1, net2, mos_score)
+		mos_score.set_shape((self.n_images, ))
 
-        optimizer = tf.train.AdamOptimizer(self.lr)
-        self.train_op = optimizer.minimize(self.loss_op)
+		net = self.block(net, 32)
+		net = self.block(net, 64)
+		net = self.block(net, 128)
+		net = self.block(net, 256)
+		net = self.block(net, 512)
 
-        
-    def patchwise_loss(self, h, t):
-        t = U.repeat(t, self.n_patches//self.n_images)
-        loss = tf.reduce_sum(tf.abs(h - t))
-        return loss
+		net1 = tf.reshape(net, (-1, 512))
+		net1 = U.dense(net1, 512, 'fc1')
+		net1 = U.swish(net1)
+		net1 = tf.nn.dropout(net1, keep_prob = self.prob)
+		net1 = U.dense(net1, 1, 'fc2')
 
-    def weighted_loss(self, h, a, t):
-        loss = 0
-        self.output = 0        
-        h = tf.split(h, self.n_images, 0)
-        a = tf.split(a, self.n_images, 0)
-        t = tf.split(t, self.n_images, 0)
+		if self.loss == "patchwise":
+			mos_score = tf.tile(mos_score, self.n_patches)
+			self.loss_op = self.patchwise_loss(net1, mos_score)
+			self.output = net1
 
-        for i in range(self.n_images):
-            y = tf.reduce_sum(h[i] * a[i], 0) /  tf.reduce_sum(a[i], 0)
-            self.output += y
-            loss += tf.abs(y - t[i])
-        
-        loss /= self.n_images
-        
-        return loss
+		elif self.loss == "weighted":
+			net2 = tf.reshape(net, (-1, 512))
+			net2 = U.dense(net2, 512, 'fc1_weight')
+			net2 = U.swish(net2)
+			net2 = tf.nn.dropout(net2, keep_prob = self.prob)
+			net2 = U.dense(net2, 1, 'fc2_weight')
+			net2 = tf.nn.relu(net2) + 1e-6
+			self.loss_op = self.weighted_loss(net1, net2, mos_score)
+
+		optimizer = tf.train.AdamOptimizer(self.lr)
+		self.train_op = optimizer.minimize(self.loss_op)
+
+	def patchwise_loss(self, h, t):
+		t = U.repeat(t, self.n_patches // self.n_images)
+		return tf.reduce_sum(tf.abs(h - t))
+
+	def weighted_loss(self, h, a, t):
+		loss = 0
+		self.output = 0
+		h = tf.split(h, self.n_images, 0)
+		a = tf.split(a, self.n_images, 0)
+		t = tf.split(t, self.n_images, 0)
+
+		for i in range(self.n_images):
+			y = tf.reduce_sum(h[i] * a[i], 0) / tf.reduce_sum(a[i], 0)
+			self.output += y
+			loss += tf.abs(y - t[i])
+
+		loss /= self.n_images
+		return loss
--- train.py
+++ train.py
@@ -1,37 +1,42 @@
-from network import Model
 import tensorflow as tf
-import numpy as np
+
 import logger
-from os.path import basename, splitext
-import matplotlib.pyplot as plt
+from network import Model
 
 EPOCHS = 1
-BATCHES = 1
-filenames = tf.constant(["test_images/macula.jpg"], dtype=tf.string)
-labels = tf.constant([1], dtype=tf.float32)
+BATCHES = 32
+filenames = tf.constant(["test_images/macula.jpg"], dtype = tf.string)
+labels = tf.constant([1], dtype = tf.float32)
 
 logger.configure('/tmp')
 sess = tf.Session()
+
+
+def _build_dataset(_filenames, _labels, epochs, batches):
+	dataset = tf.data.Dataset.from_tensor_slices((_filenames, _labels))
+	dataset = dataset.prefetch(100)
+	dataset = dataset.map(_parse_function, 10)
+	dataset = dataset.shuffle(100)
+	dataset = dataset.repeat(epochs)
+	dataset = dataset.batch(batches)
+	return dataset
+
+
 def _parse_function(filename, label):
-    image_string = tf.read_file(filename)
-    image_decoded = tf.image.decode_jpeg(image_string, channels=3)
-    image_resized = tf.image.resize_images(image_decoded, [256, 256])
-    image_normal = tf.image.per_image_standardization(image_resized)
-    image_ran_lr = tf.image.random_flip_left_right(image_normal)
-    image_ran_ud = tf.image.random_flip_up_down(image_ran_lr)
-    image_expand_dims = tf.reshape(image_ran_ud, shape=[1, -1])
-    return image_ran_ud, label
+	image_string = tf.read_file(filename)
+	image_decoded = tf.image.decode_jpeg(image_string, channels = 3)
+	image_resized = tf.image.resize_images(image_decoded, [256, 256])
+	image_normal = tf.image.per_image_standardization(image_resized)
+	image_ran_lr = tf.image.random_flip_left_right(image_normal)
+	image_ran_ud = tf.image.random_flip_up_down(image_ran_lr)
+	image_expand = tf.expand_dims(image_ran_ud, 0)
+	patches = tf.extract_image_patches(image_expand, [1, 32, 32, 1], [1, 32, 32, 1], [1, 1, 1, 1], 'SAME')[0]
+	patches = tf.reshape(patches, [-1, 32, 32, 3])
+	return patches, label
+
+
+train_data = _build_dataset(filenames, labels, EPOCHS, BATCHES)
+iterator = train_data.make_one_shot_iterator()
 
 model = Model(BATCHES, 'weighted')
-
-dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
-dataset = dataset.prefetch(100)
-dataset = dataset.map(_parse_function, 10)
-dataset = dataset.shuffle(100)
-dataset = dataset.repeat(EPOCHS)
-dataset = dataset.batch(BATCHES)
-
-itere = dataset.make_one_shot_iterator()
-ele = itere.get_next()
-print(ele)
-model.build(ele[0], ele[1])
+model.build(*iterator.get_next())
