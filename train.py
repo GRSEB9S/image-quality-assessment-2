428c8e9cf567cc18634a9b03500f6249116ce583
Vishnu Dasu <vishnu98dasu@gmail.com>
2018-02-19 10:22:34 +0530

    Update drop out prob

--- train.py
+++ train.py
@@ -66,7 +66,7 @@ sess.run(tf.global_variables_initializer())
 saver = tf.train.Saver()
 
 training_feed = {model.lr: LEARNING_RATE, model.prob: DROPOUT_PROB}
-testing_feed = {model.prob: DROPOUT_PROB}
+testing_feed = {model.prob: 1.0}
 
 for epoch in range(EPOCHS):
 	sess.run(training_init_op)
